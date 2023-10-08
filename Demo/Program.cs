6ceb2d18d2731fd8ee3fa8836d1e329d22fab245
AlexanderJordanov <tafdokidoki9708@gmail.com>
2023-10-08 20:54:03 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] firstArray = new string[n];
+string[] secondArray = new string[n];
+
+bool isFirst = true;
+
+for  (int i = 0; i < n; i++)
+{
+    string number = Console.ReadLine();
+
+    string[] numbersArray = number.Split();
+
+    if (isFirst)
+    {
+        firstArray[i] = numbersArray[0];
+        secondArray[i] = numbersArray[1];
+    }
+    else
+    {
+        firstArray[i] = numbersArray[1];
+        secondArray[i] = numbersArray[0];
+    }
+
+    isFirst = !isFirst;
+}
+Console.WriteLine(string.Join(" ", firstArray));
+Console.WriteLine(string.Join(" ", secondArray));
